153e22b8efbbf5e4219cad7505db8820f696c44c
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-05 17:20:16 -0500

    add bootstrap, font-awesome, calendar bootply template

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.1.6'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
@@ -37,6 +37,9 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
+gem 'font-awesome-rails'
 gem 'quiet_assets'
 gem 'fullcalendar-rails'
+gem 'bootstrap-sass', '~> 3.3.0.1'
+gem 'sass-rails', '~> 4.0.3'
+gem 'autoprefixer-rails'
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -4,21 +4,32 @@
 
 $(document).ready(function() {
     $('#calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+        },
         events: '/events.json',
+        editable: true,
+        eventDrop: function(event, delta, revertFunc) {
+          alert(event.title + " was dropped on " + event.start.format());
 
-        eventBackgroundColor: 'rgb(255,0,0)',
-
-        eventTextColor: 'white',
-
-        dayClick: function(date, jsEvent, view) {
-          $(this).css('background-color', 'green')
+          if (!confirm("Are you sure about this change?")) {
+            revertFunc();
+          }
         }
+    });
 
-     });
+    // $('#calendar').fullCalendar({ events: '/events.json' });
 
+        // eventBackgroundColor: 'rgb(255,0,0)',
+        // eventTextColor: 'white',
 
+        // dayClick: function(date, jsEvent, view) {
+        //   $(this).css('background-color', 'green')
+        // }
 
-
+     // });
 
 
 });
